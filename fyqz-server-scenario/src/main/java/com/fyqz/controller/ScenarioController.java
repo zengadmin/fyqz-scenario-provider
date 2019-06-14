240802aabaf0c005926252f1a7af22da3c868af9
1158553513@qq.com <1158553513@qq.com>
2019-06-14 17:18:20 +0800

    345

--- fyqz-server-scenario/src/main/java/com/fyqz/controller/ScenarioController.java
+++ fyqz-server-scenario/src/main/java/com/fyqz/controller/ScenarioController.java
@@ -3,6 +3,7 @@ package com.fyqz.controller;
 
 import com.fyqz.model.User;
 import com.fyqz.rpc.UserServiceFeign;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,12 +24,14 @@ public class ScenarioController {
      * 向前端提供一个访问地址；通过userServiceFeign调用服务并返回结果
      * @return
      */
+    @ApiOperation(value = "想定测试获取用户",notes = "想定测试获取用户")
     @RequestMapping(value = "/personScenario",method = RequestMethod.GET,produces = MediaType.APPLICATION_JSON_VALUE)
     public String person(){
         String result = userServiceFeign.getUser();
         return result;
     }
 
+    @ApiOperation(value = "测试获取想定用户",notes = "测试获想定取用户")
     @RequestMapping(value = "/userScenario",method = RequestMethod.GET,produces = MediaType.APPLICATION_JSON_VALUE)
     public String user(){
         User user = new User();
